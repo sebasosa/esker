7551d5cedbbee51a42ad8460c22b3582eda7e39f
Sebastian <sebastiansosadume@gmail.com>
2020-05-29 11:31:03 -0300

    Retoques finales del Perfil del Usuario

--- Esker1/resources/views/main/perfil.blade.php
+++ Esker1/resources/views/main/perfil.blade.php
@@ -4,24 +4,35 @@
 @section('content')
 <div class="content">
 </div>
-  <div class="container">
-    <p class="first">Perfil de Usuario</p>
-    <div class="card mb-4 perfil-user" style="max-width: 540px;" >
-      <div class="row no-gutters">
-        <div class="col-md-4 image-user">
-          <img class="card-img-top" src="{{asset('/user/'.Auth::user()->imagen)}}" alt="Avatar">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <span>Nombre: </span>
-            <h5 class="card-title">{{Auth::user()->name}}</h5>
-            <span>Email: </span>
-            <h5 class="card-text">{{Auth::user()->email}}</h5>
-            <a class="btn btn-sm btn-outline-primary fl mb-1 mr-1" href="{{url('/users/'.Auth::user()->id.'/editar')}}"> Editar</a>
+<div class="container">
+    <p class="first text-center">Perfil de Usuario</p>
+    <div class="row">
+      <div class="card col-md-6 offset-md-3 mb-5">
+        <div class="row no-gutters">
+          <div>
+            <img class="card-img-top img-user" src="{{asset('/user/'.Auth::user()->imagen)}}" alt="Avatar">
+          </div>
+          <div >
+            <div class="card-body">
+              <span>Nombre: </span>
+              <h5 class="card-title">{{Auth::user()->name}}</h5>
+              <span>Email: </span>
+              <h5 class="card-text">{{Auth::user()->email}}</h5>
+            </div>
+            <div class="card-body">
+              <a class="btn btn-sm btn-primary fl mb-1 mr-1" href="{{url('/users/'.Auth::user()->id.'/editar')}}"> Editar</a>
+              <a class="btn btn-sm btn-outline-dark agregar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+      document.getElementById('logout-form').submit();">
+                  Cerrar Sesión
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-  </div>
+</div>
 @endsection
